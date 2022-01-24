83b71b22e563b4e6db1a6c8284936a6f883a7ea6
TrojanYT <roroge.superpositivo@gmail.com>
2022-01-24 03:20:56 -0300

    ex006 - dobro_triplo_raiz

---
+++ ex006.py
@@ -0,0 +1,7 @@
+n = input('Digite um número: ')
+n1 = int(n)*2
+n2 = int(n)*3
+n3 = int(n)**(1/2)
+print('O dobro de {} é {}.'.format(n, n1))
+print('O triplo de {} é {}.'.format(n, n2))
+print('A raiz quadrada de {} é {}.'.format(n, n3))
