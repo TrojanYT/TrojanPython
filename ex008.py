e3e2d83f827249009b2efa29f32279bd5064c97a
TrojanYT <roroge.superpositivo@gmail.com>
2022-01-25 16:59:04 -0300

    ex008 - altura_m_cm_mm

---
+++ ex008.py
@@ -0,0 +1,6 @@
+n = float(input('Digite a sua altura: '))
+cm = n*100
+mm = n*1000
+print('Sua altura em metros: {}m.'.format(n))
+print('Sua altura em centimetros: {}cm.'.format(cm))
+print('Sua altura em milimetros: {}mm.'.format(mm))
