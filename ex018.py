52e4c0ed49ed678d8f443ceab53c217575f98901
TrojanYT <roroge.superpositivo@gmail.com>
2022-01-28 02:53:16 -0300

    ex018 - seno, cosseno e tangente

--- ex018.py
+++ ex018.py
@@ -1,6 +1,6 @@
 import math
 a = int(input('Informe o valor do ângulo: '))
-sen = math.sin(a)
-cos = math.cos(a)
-tg = math.tan(a)
+sen = math.sin(math.radians(a))
+cos = math.cos(math.radians(a))
+tg = math.tan(math.radians(a))
 print('A partir do ângulo {}°, o seno é {:.2f}, o cosseno é {:.2f} e a tangente é {:.2f}.'.format(a, sen, cos, tg))
