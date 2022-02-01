c8f45837cc5a33cd239dd1cc78c4e57240e55315
TrojanYT <roroge.superpositivo@gmail.com>
2022-02-01 03:59:28 -0300

    ex022 - nome de várias formas

---
+++ ex022.py
@@ -0,0 +1,6 @@
+nome = str(input('Digite o seu nome completo: '))
+print(nome.upper())
+print(nome.lower())
+print(len(nome.replace(' ','')))
+n = (nome.split())
+print('Tem {} letras no primeiro nome.'.format(len(n[0])))
